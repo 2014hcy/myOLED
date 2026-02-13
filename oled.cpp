2afb387915c71c1ed6955dbbe3704d6010725065
2014hcy <602260999@qq.com>
2026-02-14 01:50:34 +0800

    Update oled.cpp

--- oled.cpp
+++ oled.cpp
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "./oled.h"
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -18,13 +19,21 @@ void init_OLED() {
   }
   
   display.clearDisplay();
-}
-
-void write(String Text) {
-  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
+}
+
+void write(String Text) {
+  display.print(Text);
+  display.display();
+}
+
+void writeln(String Text) {
   display.println(Text);
   display.display();
+}
+
+void clear() {
+  display.clearDisplay();
 }
